31ec62604e4e4c16cf14a73d880f8d4181c50066
itslukej <me@lukej.me>
2016-09-02 20:55:23 +0100

    Add codebottle.py

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # codebottle-python
-A Python library to interact with CodeBottle's API
+A Python library to interact with CodeBottle's API.
+This is obviously still in development.
+
+##Example:
+```python
+import codebottle
+
+#Results of a search
+search = codebottle.search(keywords="java").results
+
+#Get a snippet
+snippet = codebottle.search(id="373dcc67").data
+
+#Browse
+browse = codebottle.browse(limit=10).results
+
+#Verify secure
+secure = codebottle.verifysecure(secure_token="Some type of token here")
+```
+
+##Installing
+
+From git:
+```pip install git+https://github.com/codebottle-io/codebottle-python.git```
---
+++ codebottle.py
@@ -0,0 +1,41 @@
+import requests
+
+api_version = "v1"
+api_url = "https://codebottle.io/api/{0}".format(api_version)
+
+api_search_url = "{0}/search.php".format(api_url)
+api_get_snippet_url = "{0}/get.php".format(api_url)
+api_browse_url = "{0}/browse.php".format(api_url)
+api_verifysecure_url = "{0}/verifysecure.php".format(api_url)
+
+
+class CodebottleError(Exception):
+	"""Exception to represent a error sent from the API"""
+	pass
+
+class Result(object):
+	"""A class with attributes of a codebottle API result"""
+	def __init__(self, r):
+		self.json = r.json()
+		if self.json["error"] is not "":
+			raise CodebottleError(self.json["error"])
+
+		for k, v in self.json.items():
+			setattr(self, k, v)
+
+#Functions
+def search(**kwargs):
+	result = requests.get(api_search_url, params=kwargs)
+	return Result(result)
+
+def get(**kwargs):
+	result = requests.get(api_get_snippet_url, params=kwargs)
+	return Result(result)
+
+def browse(**kwargs):
+	result = requests.get(api_browse_url, params=kwargs)
+	return Result(result)
+
+def verifysecure(**kwargs):
+	result = requests.get(api_verifysecure_url, params=kwargs)
+	return Result(result)
