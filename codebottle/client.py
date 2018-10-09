23cf67136fcb80bcbdbe0755ca99af1a736419a3
wolfy1339 <webmaster@wolfy1339.com>
2018-10-08 23:40:45 -0400

    Update to latest API and follow JS library

--- codebottle/client.py
+++ codebottle/client.py
@@ -1,66 +1,43 @@
 import requests
+import helpers
 
-from .exceptions import CodebottleError
-from .auth import Token
+headers = {
+	'Accept': 'application/vnd.codebottle.v1+json'
+}
+
+def _get(url, params=None):
+    try:
+        req = requests.get(url, headers=headers, params=params)
+        return req
+    except Exception as e:
+        raise e
+
+def search(keywords, language):
+    return _get(helpers.apiUrl('/snippets'), params={
+        keywords: keywords,
+        language: language
+    }).json()
 
 
-class Result(object):
-    '''A class with attributes of a codebottle API result'''
-    def __init__(self, r):
-        self.json = r.json()
-        if self.json.get('error'):
-            raise CodebottleError(self.json['error'])
-
-        for k, v in self.json.items():
-            setattr(self, k, v)
+def get(id):
+    return _get(helpers.apiUrl('/snippets/'.format(id)))
 
 
-class CodeBottle(object):
+def getLatest():
+    return _get(helpers.apiUrl('/snippets/new'))
 
-    def __init__(self, token=None):
-        self.api_url = 'https://api.codebottle.io'
-        self.api_accept = 'application/vnd.codebottle.v1+json'
-        self.token = token
-        self.snippets = self.Snippets(self)
 
-    def _build_url(self, endpoint, *args):
-        url = '{0}/{1}'.format(self.api_url, endpoint)
-        if len(args) > 0:
-            args = [str(a) for a in args]
-            url = '{0}/{1}'.format(url, '/'.join(args))
-        return url
+def getCategories() {
+    return _get(helpers.apiUrl('/categories'))
 
-    def _get(self, endpoint, *args, **kwargs):
-        url = self._build_url(endpoint, *args)
-        r = requests.get(url, params=kwargs, auth=Token(self.token),
-                         headers={'Accept': self.api_accept})
-        return Result(r)
 
-    def _post(self, endpoint, *args, **kwargs):
-        url = self._build_url(endpoint, *args)
-        r = requests.post(url, data=kwargs, auth=Token(self.token),
-                          headers={'Accept': self.api_accept})
-        return Result(r)
+def getCategory(id):
+    return _get(helpers.apiUrl('/categories/{0}'.format(id)))
 
-    def languages(self, *args):
-        return self._get('languages', *args)
 
-    def categories(self, *args):
-        return self._get('categories', *args)
+def getLanguages():
+    return _get(helpers.apiUrl('/languages'))
 
-    class Snippets(object):
 
-        def __init__(self, parent):
-            self.parent = parent
-
-        def get(self, *args, **kwargs):
-            return self.parent._get('snippets', *args, **kwargs)
-
-        def new(self):
-            return self.get('new')
-
-        def create(self, **kwargs):
-            return self.parent._post('snippets', **kwargs)
-
-        def vote(self, *args, **kwargs):
-            return self.parent._post('snippets', *args, **kwargs)
+def getLanguage(id):
+		return _get(helpers.apiUrl('/languages/{0}'.format(id)))
--- codebottle/exceptions.py
+++
@@ -1,3 +0,0 @@
-class CodebottleError(Exception):
-    '''Exception to represent a error sent from the API'''
-    pass
